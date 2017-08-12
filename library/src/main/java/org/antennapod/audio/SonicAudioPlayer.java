1e1fdce6cd7945107f62a0f74d9b5e93d0c8a55d
Martin Fietz <martin.fietz@gmail.com>
2017-08-12 12:38:11 +0200

    Extract method

--- library/src/main/java/org/antennapod/audio/SonicAudioPlayer.java
+++ library/src/main/java/org/antennapod/audio/SonicAudioPlayer.java
@@ -720,16 +720,7 @@ public class SonicAudioPlayer extends AbstractAudioPlayer {
                                     int maxBytes = (available / 4) * 4;
                                     mSonic.readBytesFromStream(modifiedSamples, maxBytes);
 
-                                    for (int i = 0; (i + 3) < modifiedSamples.length; i += 4) {
-                                        short left = (short) ((modifiedSamples[i] & 0xff) | (modifiedSamples[i + 1] << 8));
-                                        short right = (short) ((modifiedSamples[i + 2] & 0xff) | (modifiedSamples[i + 3] << 8));
-                                        short value = (short) (0.5 * left + 0.5 * right);
-
-                                        modifiedSamples[i] = (byte) (value & 0xff);
-                                        modifiedSamples[i + 1] = (byte) (value >> 8);
-                                        modifiedSamples[i + 2] = (byte) (value & 0xff);
-                                        modifiedSamples[i + 3] = (byte) (value >> 8);
-                                    }
+                                    downMix(modifiedSamples);
                                     mTrack.write(modifiedSamples, 0, maxBytes);
                                 } else {
                                     mSonic.readBytesFromStream(modifiedSamples, available);
@@ -854,9 +845,19 @@ public class SonicAudioPlayer extends AbstractAudioPlayer {
                 return "UNKNOWN_STATE";
         }
     }
+    private void downMix(byte[] modifiedSamples) {
+        for (int i = 0; (i + 3) < modifiedSamples.length; i += 4) {
+            short left = (short) ((modifiedSamples[i] & 0xff) | (modifiedSamples[i + 1] << 8));
+            short right = (short) ((modifiedSamples[i + 2] & 0xff) | (modifiedSamples[i + 3] << 8));
+            short value = (short) (0.5 * left + 0.5 * right);
 
     private boolean isValidStateForSeeking(int state) {
         return state == STATE_STARTED || state == STATE_PREPARED || state == STATE_PAUSED || state == STATE_PLAYBACK_COMPLETED;
+            modifiedSamples[i] = (byte) (value & 0xff);
+            modifiedSamples[i + 1] = (byte) (value >> 8);
+            modifiedSamples[i + 2] = (byte) (value & 0xff);
+            modifiedSamples[i + 3] = (byte) (value >> 8);
+        }
     }
 
 
